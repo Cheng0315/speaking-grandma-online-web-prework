3ab8d0c6aca51e0aa3b510d7eac6190fa4bb4188
Cheng Vue <cvu3@ucsc.edu>
2018-11-09 20:19:45 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,11 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(message)
-  if message == message.upcase
+  if message == 'I LOVE YOU GRANDMA!'
     'I LOVE YOU TOO PUMPKIN!'
-  elsif message != message.upcase
-    'HUH?! SPEAK UP, SONNY!'
-  else
+  elsif message == message.upcase
     "NO, NOT SINCE 1938!"
+  else
+    'HUH?! SPEAK UP, SONNY!'
   end
 end
